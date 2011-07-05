357e2b7590b53e4ec54ebc4aadbc09521961966a
Elliot Cameron <3noch@3noch.com>
2011-07-05 18:57:24 -0400

    Still working out some kinks.

--- DewhitenCore.hs
+++ DewhitenCore.hs
@@ -1,22 +1,38 @@
 module DewhitenCore where
+    data IndentOptions = UseBlanks | CopyIndent
+    
     dewhitenString :: String -> String
-    dewhitenString = unlines . dewhitenLines . lines
+    dewhitenString = unlines . (dewhitenLines CopyIndent) . lines
     
-    dewhitenLines :: [String] -> [String]
-    dewhitenLines []     = []
-    dewhitenLines [x]    = [dewhitenLine (x, "")]
-    dewhitenLines (x:xs) = dewhitenLine (x, head xs) : dewhitenLines xs
+    dewhitenLines :: IndentOptions -> [String] -> [String]
+    dewhitenLines UseBlanks  = (map trimLine) . trimList
+    dewhitenLines CopyIndent = indentLines . (map trimNonWhiteLine) . trimList
     
-    dewhitenLine :: (String, String) -> String
-    dewhitenLine linePair
-        | all isWhite (fst linePair) = takeWhile isWhite (snd linePair)
-        | otherwise = trimLine (fst linePair)
+    indentLines :: [String] -> [String]
+    indentLines []  = []
+    indentLines [x] = [trimLine x]
+    indentLines xs  = indentLines (init newList)
+                      where newList = take lenLess2 xs ++ indentPair (penultimate xs) (last xs)
+                            lenLess2 = length xs - 2
+                            penultimate = last . init
+    
+    indentPair :: String -> String -> [String]
+    indentPair a b = [if isWhiteLine a then takeWhile isWhite b else a,
+                      if isWhiteLine b then b else b]
+    
+    trimNonWhiteLine :: String -> String
+    trimNonWhiteLine a
+        | isWhiteLine a = a
+        | otherwise     = trimLine a
     
     trimLine :: String -> String
     trimLine = reverse . dropWhile isWhite . reverse
     
-    trimFile :: [String] -> [String]
-    trimFile = reverse . dropWhile null . reverse
+    trimList :: [String] -> [String]
+    trimList = reverse . dropWhile isWhiteLine . reverse
+    
+    isWhiteLine :: String -> Bool
+    isWhiteLine = all isWhite
     
     isWhite :: Char -> Bool
     isWhite ' '  = True
